ea9afb809117f0dec350f2652634bd53eb339c61
leanjperalta <lean.j.peralta@gmail.com>
2024-09-17 08:52:51 -0300

    Mod validator para validacion de datos

--- FastAPI/main_example2.py
+++ FastAPI/main_example2.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel,Field
+from pydantic import BaseModel,Field, validator
 from typing import Optional, List
 import datetime
+
+#Versión usando validator module para validar datos y responder msg personalizados.
+#Se reemplaza al método field, en este caso al elemento title en la clase CreateMovie
 
 app = FastAPI() 
 
@@ -16,7 +19,7 @@ class Movie(BaseModel):
 
 class CreateMovie(BaseModel):
     id: int #Optional[int] = None o se puede declarar id: int | None = None
-    title: str = Field(min_length=5, max_length=15) 
+    title: str 
     overview: str = Field(min_length=5, max_length=50) #Valido condiciones de los datos
     year: int = Field(le=datetime.date.today().year) # el año debe ser menor o igual al año actual
     rating: float = Field(ge=0, le=10)
@@ -34,6 +37,15 @@ class CreateMovie(BaseModel):
             }
         }
     }
+
+    @validator('title')
+    def validate_title(cls, value): #Valido title, recibo clase y valor de title.
+        if len(value) < 5:
+            raise ValueError('Title field must have a min. of 5 chars')
+        if len(value) > 15:
+            raise ValueError('Title field must have a max. of 15 chars')
+        
+
 
 class MovieUpdate(BaseModel):
     title: str
@@ -80,21 +92,21 @@ def get_movies() -> List[Movie]: #muestro una lista de tipo Movie (indico tipo d
 
 @app.get('/movies/{id}', tags=['Movies']) # método get - ruta /movies
 
-def get_movie(id: int) -> Movie: #muestro un objeto de tipo Movie (indico tipo de respuesta/schema)
+def get_movie(id: int = Path(gt=0)) -> Movie | dict: #muestro un objeto de tipo Movie (indico tipo de respuesta/schema), valido que el parametro id se mayor o igual a 0 (mod Path), devuelve una Movie o un diccionario en caso de no encontrar dato alguno
     for movie in movies:
-        if movie['id']== id:
+        if movie.id == id: 
             return movie.dict() #convierto en dict
     
-    return []
+    return {} #devuelvo un objeto vacío ya que no hay mas listas para devolver (return [])
 
 @app.get('/movies/', tags=['Movies']) # método get - ruta /movies
 
-def get_movie_by_category(category: str, year: int) -> Movie:
+def get_movie_by_category(category: str = Query(min_length=5)) -> Movie | dict: #Validación de parámetro query
       for movie in movies:
         if movie['category']== category:
-            return movie.dict()
+            return movie.category
     
-      return [] 
+      return {}
 
 
 @app.post('/movies', tags=['Movies']) # método post - ruta /movies
