1d2e951ca24c09bec25a86d195e4f4848527d5e5
leanjperalta <lean.j.peralta@gmail.com>
2025-03-25 11:02:15 -0300

    folder cleanse

--- ActiveDirectory/Create_User_API_OAuth2/vars/conn_creds.py
+++
@@ -1,7 +0,0 @@
-import os
-
-server_name = 'ldap://hpg-ad.garrahan.gov.ar'
-domain_name = 'garrahan.gov.ar'
-admin_user = 'lperalta'
-admin_password = os.environ.get('LP_PASS')
-base_dn = 'OU=DSOPORTE,OU=GSISTEMAS,OU=ADM,DC=garrahan,DC=gov,DC=ar'
