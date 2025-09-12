097386addf54b3c23a2dce4d7cea844e68b66cf3
Leandro Peralta <90806231+leanjperalta@users.noreply.github.com>
2025-09-12 15:37:32 -0300

    Update ADuser_create.py

--- ActiveDirectory/ADuser_create.py
+++ ActiveDirectory/ADuser_create.py
@@ -2,11 +2,11 @@ from ldap3 import Server, Connection, ALL, NTLM, MODIFY_ADD
 from vars.conn_creds import *
 
 # User details
-new_user_name = "John"
-new_user_lastname = "Doe"
+new_user_name = "Juan"
+new_user_lastname = "Perez"
 new_user_cn = f"{new_user_name} {new_user_lastname}"
 new_user_sam = f"{new_user_name[:1].upper()}{new_user_lastname.lower()}"
-new_user_password = 'Password123!'
+new_user_password = 'xxxxxxx'
 
 def connect_to_ad():
     server = Server(server_name, get_info=ALL)
@@ -20,16 +20,16 @@ def create_user(conn, base_dn, user_cn, user_sam, user_password):
     user_dn = f'CN={user_cn},{base_dn}'
     user_exists = conn.search(user_dn, '(objectClass=user)', attributes=['*'])
     if user_exists:
-        print(f"User {user_cn} already exists.")
+        print(f"Usuario {user_cn} ya existe.")
         return
     
     attributes = {
         'cn': user_cn,
         'sAMAccountName': user_sam,
         'userPrincipalName': f'{user_sam}@{domain_name}',
-        'userAccountControl': 512,  # Normal account
-        'givenName': 'John',
-        'sn': 'Doe',
+        'userAccountControl': 512, 
+        'givenName': 'Juan',
+        'sn': 'Perez',
         'displayName': user_cn,
         'mail': f'{user_sam}@{domain_name}',
     #    'userPassword': user_password
@@ -40,7 +40,7 @@ def create_user(conn, base_dn, user_cn, user_sam, user_password):
         # Enable the user and set the password
         conn.extend.microsoft.modify_password(user_dn, user_password)
         conn.modify(user_dn, {'userAccountControl': [(MODIFY_ADD, [512])]})
-        print("Password set and user enabled.")
+        print("Usuario y contraseña habilitada.")
     else:
         print(f"Failed to create user: {conn.result}")
 
